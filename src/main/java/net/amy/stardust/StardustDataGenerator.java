57414bb5a5d3eda80213b26e648d6e2346247442
amythest7 <inb45@outlook.com>
2026-05-05 18:47:47 +0100

    Initial commit

---
+++ src/main/java/net/amy/stardust/StardustDataGenerator.java
@@ -0,0 +1,11 @@
+package net.amy.stardust;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StardustDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
